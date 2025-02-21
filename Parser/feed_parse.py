89cceca653524f0fd61b8ef2b7154e18853e3ef9
jabovik <mojabovik@gmail.com>
2025-02-21 15:29:34 +0400

    Format

--- Parser/feed_parse.py
+++ Parser/feed_parse.py
@@ -1,13 +1,16 @@
 import feedparser
+
+
 class Enclosure:
     def __init__(self, enclosure):
         self.link = enclosure.url
         self.type = enclosure.type
-        
+
+
 class FeedArticle:
     def __init__(self, link, published, title, summary, enclosures, source):
         self.link = link
-        self.published = published # tuple(YEAR, MONTH, DAY, HOUR, MINUTE, SECOND, WEEK DAY(0-6), YEAR DAY, DST flag)
+        self.published = published  # tuple(YEAR, MONTH, DAY, HOUR, MINUTE, SECOND, WEEK DAY(0-6), YEAR DAY, DST flag)
         self.title = title
         self.summary = summary
         self.enclosures = enclosures
@@ -15,20 +18,23 @@ class FeedArticle:
         self.source = source
         self.embedding = None
 
+
 class FeedParser:
-    def __init__(self,url):
-        self.url =url
-        
+    def __init__(self, url):
+        self.url = url
+
     def feed_parse(self) -> list[FeedArticle]:
         articles = []
         rss = feedparser.parse(self.url)
         for entry in rss.entries:
-            articles.append(FeedArticle(
-                link= entry.link,
-                published = str(entry.published_parsed),
-                title= entry.title,
-                summary= entry.summary,
-                enclosures = [Enclosure(i) for i in entry.enclosures],
-                source = rss.feed.title
-            ))
+            articles.append(
+                FeedArticle(
+                    link=entry.link,
+                    published=str(entry.published_parsed),
+                    title=entry.title,
+                    summary=entry.summary,
+                    enclosures=[Enclosure(i) for i in entry.enclosures],
+                    source=rss.feed.title,
+                )
+            )
         return articles
