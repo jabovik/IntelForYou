470be1d96a951a34b944834388553e2e77f47a81
jabovik <mojabovik@gmail.com>
2025-02-21 15:29:34 +0400

    Format

--- DB/IntelDB.py
+++ DB/IntelDB.py
@@ -1,18 +1,21 @@
 import sqlite3
 import datetime
 
-class DB():
-    def __init__(self,db_name):
+
+class DB:
+    def __init__(self, db_name):
         self.con = sqlite3.connect(db_name)
         self.cur = self.con.cursor()
         self.con.autocommit = True
         self._create_tables()
+
     def __del__(self):
         self.con.close()
-    
+
     def _create_tables(self):
-        #Table of articles fetched from RSS
-        self.cur.execute("""
+        # Table of articles fetched from RSS
+        self.cur.execute(
+            """
             CREATE TABLE IF NOT EXISTS articles(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 source TEXT NOT NULL,
@@ -25,20 +28,24 @@ class DB():
                 embedding TEXT
             )
             
-        """)
+        """
+        )
         # Table of enclosures
-        self.cur.execute("""
+        self.cur.execute(
+            """
             CREATE TABLE IF NOT EXISTS media(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 article_id INTEGER NOT NULL,
                 type TEXT NOT NULL,
                 link TEXT NOT NULL
             )
-        """)
-        print('tables created!')
-        
-    def _add_feed_article(self,article):
-        self.cur.execute("""
+        """
+        )
+        print("tables created!")
+
+    def _add_feed_article(self, article):
+        self.cur.execute(
+            """
             INSERT INTO articles(title, description,link,type,embedding, source,published, inserted)
             VALUES(?,?,?,?,?,?,?,?)
             """,
@@ -49,33 +56,32 @@ class DB():
                 article.type,
                 article.embedding,
                 article.source,
-                article.published, #Those datetime formats are different
-                str(datetime.datetime.now()) # Probably needs to be fixed
-            )
+                article.published,  # Those datetime formats are different
+                str(datetime.datetime.now()),  # Needs to be fixed
+            ),
         )
         id = self.cur.lastrowid
         for enclosure in article.enclosures:
-            self.cur.execute("""
+            self.cur.execute(
+                """
                 INSERT INTO media(article_id,type,link)
                 VALUES(?,?,?)
                 """,
-                (
-                    id,
-                    enclosure.type, # Probably does not works
-                    enclosure.link
-                )
+                (id, enclosure.type, enclosure.link),
             )
-    def _add_telegram_article(self,article):
+
+    def _add_telegram_article(self, article):
         pass
-    def add_article(self,article):
-        method_name = f'_add_{str(article.type)}_article'
+
+    def add_article(self, article):
+        method_name = f"_add_{str(article.type)}_article"  # method is called the same as the type of an article
         method = getattr(self, method_name)
         method(article)
-        
+
     def show_articles(self):
-        self.cur.execute('SELECT * FROM articles')
+        self.cur.execute("SELECT * FROM articles")
         return self.cur.fetchall()
-    
+
     def delete_all(self):
-        self.cur.execute('DELETE FROM articles')
-        self.cur.execute('DELETE FROM media')
+        self.cur.execute("DELETE FROM articles")
+        self.cur.execute("DELETE FROM media")
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import DB.IntelDB
 import Parser.feed_parse
-db = DB.IntelDB.DB('IFY.db')
+
+db = DB.IntelDB.DB("IFY.db")
 db.delete_all()
-with open('rss.txt', 'r') as f:
+with open("rss.txt", "r") as f:
     rss_links = f.readlines()
     for link in rss_links:
         parser = Parser.feed_parse.FeedParser(link)
         articles = parser.feed_parse()
         for article in articles:
             db.add_article(article)
-print (db.show_articles())
+articles = db.show_articles()
+for i in articles:
+    print(i)
